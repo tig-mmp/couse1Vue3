2e662586e9dd2cc5e9e3667b392b0247dd00f3d4
Tiago Pinto <tigmmp@hotmail.com>
2020-11-22 11:58:03 +0000

    User Interactions with Methods

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = createApp({
   template: `
 		<h1>Hello {{msg}}</h1>
 		<p>{{person}}</p>
+
+		<h2>Increment</h2>
+		<button v-on:click="increment()">Increment</button>
+		<p>{{count}}</p>
 	`,
   data() {
     return {
@@ -12,6 +16,12 @@ const app = createApp({
         name: "Tiago Pinto",
         age: 23,
       },
+      count: 0,
     };
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    },
   },
 }).mount("#app");
